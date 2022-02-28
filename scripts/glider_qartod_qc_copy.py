9fe1234394d041b512df2a1f8f88983b51a37098
Lori Garzio <lgarzio@marine.rutgers.edu>
2022-02-27 19:50:09 -0500

    remove testing stuff

--- scripts/glider_qartod_qc_copy.py
+++ scripts/glider_qartod_qc_copy.py
@@ -2,7 +2,7 @@
 
 """
 Author: lnazzaro and lgarzio on 12/7/2021
-Last modified: lnazzaro on 2/25/2022
+Last modified: lgarzio on 2/18/2022
 Run ioos_qc QARTOD tests on processed glider NetCDF files and append the results to the original file.
 """
 
@@ -126,22 +126,26 @@ def set_qartod_attrs(test, sensor, thresholds):
     return attrs
 
 
-def main(args):
+#def main(args):
+def main(deployments, mode, cdm_data_type, loglevel, dataset_type, test):
     status = 0
 
-    loglevel = args.loglevel.upper()
-    cdm_data_type = args.cdm_data_type
-    mode = args.mode
-    dataset_type = args.level
-    test = args.test
+    # loglevel = args.loglevel.upper()
+    # cdm_data_type = args.cdm_data_type
+    # mode = args.mode
+    # dataset_type = args.level
+    # test = args.test
+    loglevel = loglevel.upper()
 
-    logFile_base = logfile_basename()
+    logFile_base = os.path.join(os.path.expanduser('~'), 'glider_qc_log')  # for debugging
+    #logFile_base = logfile_basename()
     logging_base = setup_logger('logging_base', loglevel, logFile_base)
 
     data_home, deployments_root = find_glider_deployments_rootdir(logging_base, test)
     if isinstance(deployments_root, str):
 
-        for deployment in args.deployments:
+        #for deployment in args.deployments:
+        for deployment in [deployments]:
 
             data_path, deployment_location = find_glider_deployment_datapath(logging_base, deployment, deployments_root,
                                                                              dataset_type, cdm_data_type, mode)
@@ -416,38 +420,45 @@ def main(args):
 
 
 if __name__ == '__main__':
-    arg_parser = argparse.ArgumentParser(description=main.__doc__,
-                                         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-    arg_parser.add_argument('deployments',
-                            nargs='+',
-                            help='Glider deployment name(s) formatted as glider-YYYYmmddTHHMM')
-
-    arg_parser.add_argument('-m', '--mode',
-                            help='Deployment dataset status',
-                            choices=['rt', 'delayed'],
-                            default='rt')
-
-    arg_parser.add_argument('--level',
-                            choices=['sci', 'ngdac'],
-                            default='sci',
-                            help='Dataset type')
-
-    arg_parser.add_argument('-d', '--cdm_data_type',
-                            help='Dataset type',
-                            choices=['profile'],
-                            default='profile')
-
-    arg_parser.add_argument('-l', '--loglevel',
-                            help='Verbosity level',
-                            type=str,
-                            choices=['debug', 'info', 'warning', 'error', 'critical'],
-                            default='info')
-
-    arg_parser.add_argument('-test', '--test',
-                            help='Point to the environment variable key GLIDER_DATA_HOME_TEST for testing.',
-                            action='store_true')
-
-    parsed_args = arg_parser.parse_args()
-
-    sys.exit(main(parsed_args))
+    deploy = 'ru30-20210226T1647'  # maracoos_02-20210716T1814 ru34-20200729T1430 ru33-20201014T1746 ru33-20200715T1558  ru32-20190102T1317 ru30-20210503T1929
+    mode = 'rt'
+    d = 'profile'
+    ll = 'info'
+    level = 'sci'
+    test = True
+    main(deploy, mode, d, ll, level, test)
+    # arg_parser = argparse.ArgumentParser(description=main.__doc__,
+    #                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    #
+    # arg_parser.add_argument('deployments',
+    #                         nargs='+',
+    #                         help='Glider deployment name(s) formatted as glider-YYYYmmddTHHMM')
+    #
+    # arg_parser.add_argument('-m', '--mode',
+    #                         help='Deployment dataset status',
+    #                         choices=['rt', 'delayed'],
+    #                         default='rt')
+    #
+    # arg_parser.add_argument('--level',
+    #                         choices=['sci', 'ngdac'],
+    #                         default='sci',
+    #                         help='Dataset type')
+    #
+    # arg_parser.add_argument('-d', '--cdm_data_type',
+    #                         help='Dataset type',
+    #                         choices=['profile'],
+    #                         default='profile')
+    #
+    # arg_parser.add_argument('-l', '--loglevel',
+    #                         help='Verbosity level',
+    #                         type=str,
+    #                         choices=['debug', 'info', 'warning', 'error', 'critical'],
+    #                         default='info')
+    #
+    # arg_parser.add_argument('-test', '--test',
+    #                         help='Point to the environment variable key GLIDER_DATA_HOME_TEST for testing.',
+    #                         action='store_true')
+    #
+    # parsed_args = arg_parser.parse_args()
+    #
+    # sys.exit(main(parsed_args))
