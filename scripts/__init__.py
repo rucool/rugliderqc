f573e9368b1c08e016734a4d7d7ab16241c5b77e
Lori Garzio <lgarzio@marine.rutgers.edu>
2022-03-02 16:01:49 -0500

    remove test of hysteresis test

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,7 +1,6 @@
 from . import check_duplicate_timestamps
 from . import check_science_variables
 from . import ctd_hysteresis_test
-from . import ctd_hysteresis_test_copy
 from . import glider_qartod_qc
 from . import move_nc_files
 from . import summarize_qartod_flags
