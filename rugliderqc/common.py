379bf86d53282a7544f88bd20ec2faf6890bfb2a
Lori Garzio <lgarzio@marine.rutgers.edu>
2022-12-16 16:43:13 -0500

    fix finding glider names/dates in deployment names

--- rugliderqc/common.py
+++ rugliderqc/common.py
@@ -1,42 +1,50 @@
 #!/usr/bin/env python
 
 import os
+import re
 import pytz
 from dateutil import parser
 
 
 def find_glider_deployment_datapath(logger, deployment, deployments_root, dataset_type, cdm_data_type, mode):
-    try:
-        (glider, trajectory) = deployment.split('-')
+    glider_regex = re.compile(r'^(.*)-(\d{8}T\d{4})')
+    match = glider_regex.search(deployment)
+    if match:
         try:
-            trajectory_dt = parser.parse(trajectory).replace(tzinfo=pytz.UTC)
-        except ValueError as e:
-            logger.error('Error parsing trajectory date {:s}: {:}'.format(trajectory, e))
-            trajectory_dt = None
-            data_path = None
-            deployment_location = None
-
-        if trajectory_dt:
-            trajectory = '{:s}-{:s}'.format(glider, trajectory_dt.strftime('%Y%m%dT%H%M'))
-            deployment_name = os.path.join('{:0.0f}'.format(trajectory_dt.year), trajectory)
-
-            # Create fully-qualified path to the deployment location
-            deployment_location = os.path.join(deployments_root, deployment_name)
-            if os.path.isdir(deployment_location):
-                # Set the deployment netcdf data path
-                data_path = os.path.join(deployment_location, 'data', 'out', 'nc',
-                                         '{:s}-{:s}/{:s}'.format(dataset_type, cdm_data_type, mode))
-                if not os.path.isdir(data_path):
-                    logger.warning('{:s} data directory not found: {:s}'.format(trajectory, data_path))
-                    data_path = None
-                    deployment_location = None
-            else:
-                logger.warning('Deployment location does not exist: {:s}'.format(deployment_location))
+            (glider, trajectory) = match.groups()
+            try:
+                trajectory_dt = parser.parse(trajectory).replace(tzinfo=pytz.UTC)
+            except ValueError as e:
+                logger.error('Error parsing trajectory date {:s}: {:}'.format(trajectory, e))
+                trajectory_dt = None
                 data_path = None
                 deployment_location = None
 
-    except ValueError as e:
-        logger.error('Error parsing invalid deployment name {:s}: {:}'.format(deployment, e))
+            if trajectory_dt:
+                trajectory = '{:s}-{:s}'.format(glider, trajectory_dt.strftime('%Y%m%dT%H%M'))
+                deployment_name = os.path.join('{:0.0f}'.format(trajectory_dt.year), trajectory)
+
+                # Create fully-qualified path to the deployment location
+                deployment_location = os.path.join(deployments_root, deployment_name)
+                if os.path.isdir(deployment_location):
+                    # Set the deployment netcdf data path
+                    data_path = os.path.join(deployment_location, 'data', 'out', 'nc',
+                                             '{:s}-{:s}/{:s}'.format(dataset_type, cdm_data_type, mode))
+                    if not os.path.isdir(data_path):
+                        logger.warning('{:s} data directory not found: {:s}'.format(trajectory, data_path))
+                        data_path = None
+                        deployment_location = None
+                else:
+                    logger.warning('Deployment location does not exist: {:s}'.format(deployment_location))
+                    data_path = None
+                    deployment_location = None
+
+        except ValueError as e:
+            logger.error('Error parsing invalid deployment name {:s}: {:}'.format(deployment, e))
+            data_path = None
+            deployment_location = None
+    else:
+        logger.error('Cannot pull glider name from {:}'.format(deployment))
         data_path = None
         deployment_location = None
 
