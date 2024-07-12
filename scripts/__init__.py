d506f47b43df33f6ea412b044dcf0d63d968207b
Lori Garzio <lgarzio@marine.rutgers.edu>
2024-07-12 14:55:34 -0400

    clean up files

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -5,6 +5,5 @@ from . import ctd_hysteresis_test
 from . import glider_qartod_qc
 from . import interpolate_depth
 from . import move_nc_files
-from . import specify_attributes
 from . import summarize_qartod_flags
 from . import time_shift
